9bc74e11ffdc27be4112b4e0df3d5d3f0ca67742
mrgucci1 <dalquist.keaton221@gmail.com>
2022-01-23 15:59:30 -0600

    Added Problem, 752, open the lock. medium difficulty. Used Neetcode video to help understand, and c# discussion posts to help write working c# code

--- LeetCode_Practive/Program.cs
+++ LeetCode_Practive/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,34 +11,9 @@ namespace LeetCode_Practive
     {
         static void Main(string[] args)
         {
-            //https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
-            int[] nums = { 4,3,2,7,8,2,3,1};
-            List<int> ans = FindDisappearedNumbers(nums);
-            for (int i = 0; i < ans.Count(); i++)
-            {
-                Console.WriteLine(ans[i]);
-            }
+            //link goes here
+            Console.WriteLine("Ready for next problem, Sire!");
             Console.ReadKey();
-        }
-        static List<int> FindDisappearedNumbers(int[] nums)
-        {
-            //for O(n) and O(1) we modifiy given array by marking digts found with a negative
-            //we mark the index with a negative but dont replace the value. so for EX:
-            //if we find 4 in the array, we mark index 3(0-index) with negative. That means 4 exits. 
-            //any index left postiive meants index + 1 is a disappeared number
-            for (int i = 0; i < nums.Length; i++)
-            {
-                int index = Math.Abs(nums[i]);
-                nums[index - 1] = Math.Abs(nums[index - 1]) * -1;
-            }
-            List<int> missingNum = new List<int>();
-            for(int i = 0;i<nums.Length;i++)
-            {
-                //if postive, add to missingNum array
-                if (nums[i] > 0)
-                    missingNum.Add(i+1);
-            }
-            return missingNum;
         }
     }
 }
---
+++ LeetCode_Practive/problem752.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Practive
+{
+    class problem752
+    {
+        public void Main752(string[] args)
+        {
+            //https://leetcode.com/problems/open-the-lock/
+            string[] deadends = { "0201", "0101", "0102", "1212", "2002" };
+            string target = "0202";
+            int ans = OpenLock(deadends, target);
+            Console.WriteLine(ans);
+            Console.ReadKey();
+        }
+        public int OpenLock(string[] deadends, string target)
+        {
+            //check if it is possible to find target
+            if (deadends.Contains("0000") || deadends.Contains(target))
+                return -1;
+            //create hashset to store strings we have visited. start with deadends because we cant visit those
+            HashSet<string> visited = new HashSet<string>(deadends);
+            //use q to find target
+            Queue q = new Queue();
+            q.Enqueue("0000");
+            visited.Add("0000");
+            int level = 0;
+            //while q has elements, loop for target
+            while (q.Count > 0)
+            {
+                int count = q.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    //pop off first item in q
+                    var current = q.Dequeue();
+                    if (current.Equals(target))
+                        return level;
+                    //check all children of current string, add to visited and add to q if not already there or deadend
+                    foreach (var child in GetChildren(current.ToString()))
+                    {
+                        if (!(visited.Contains(child)))
+                        {
+                            q.Enqueue(child);
+                            visited.Add(child);
+                        }
+                    }
+                }
+                level++;
+            }
+            return -1;
+        }
+
+        public List<string> GetChildren(string current)
+        {
+            List<string> result = new List<string>();
+            for (int i = 0; i < current.Length; i++)
+            {
+                char[] arr1 = current.ToCharArray(), arr2 = current.ToCharArray();
+                arr1[i] = arr1[i] == '9' ? '0' : (char)((int)arr1[i] + 1);
+                result.Add(new string(arr1));
+                arr2[i] = arr2[i] == '0' ? '9' : (char)((int)arr2[i] - 1);
+                result.Add(new string(arr2));
+            }
+            return result;
+        }
+    }
+}
