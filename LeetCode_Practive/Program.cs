8a6d7226e79600bb3245928ee32a5e0aea72743a
mrgucci1 <dalquist.keaton221@gmail.com>
2022-01-23 14:44:55 -0600

    Add project files.

---
+++ LeetCode_Practive/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Practive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+            int[] nums = { 4,3,2,7,8,2,3,1};
+            List<int> ans = FindDisappearedNumbers(nums);
+            for (int i = 0; i < ans.Count(); i++)
+            {
+                Console.WriteLine(ans[i]);
+            }
+            Console.ReadKey();
+        }
+        static List<int> FindDisappearedNumbers(int[] nums)
+        {
+            //for O(n) and O(1) we modifiy given array by marking digts found with a negative
+            //we mark the index with a negative but dont replace the value. so for EX:
+            //if we find 4 in the array, we mark index 3(0-index) with negative. That means 4 exits. 
+            //any index left postiive meants index + 1 is a disappeared number
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int index = Math.Abs(nums[i]);
+                nums[index - 1] = Math.Abs(nums[index - 1]) * -1;
+            }
+            List<int> missingNum = new List<int>();
+            for(int i = 0;i<nums.Length;i++)
+            {
+                //if postive, add to missingNum array
+                if (nums[i] > 0)
+                    missingNum.Add(i+1);
+            }
+            return missingNum;
+        }
+    }
+}
---
+++ LeetCode_Practive/problem1578.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Practive
+{
+    class problem1578
+    {
+        static void Main1579(string[] args)
+        {
+            //https://leetcode.com/problems/minimum-time-to-make-rope-colorful/
+            string colors = "bbbaaa";
+            int[] neededTime = { 4, 9, 3, 8, 8, 9 };
+            Console.WriteLine(MinCost(colors, neededTime));
+            Console.ReadKey();
+        }
+        static public int MinCost(string colors, int[] neededTime)
+        {
+
+            int result = 0, max_cost = 0, sum_cost = 0;
+            for (int i = 0; i < colors.Length; i++)
+            {
+                if (i > 0 && colors[i] != colors[i - 1])
+                {
+                    result += sum_cost - max_cost;
+                    sum_cost = max_cost = 0;
+                }
+                sum_cost += neededTime[i];
+                max_cost = Math.Max(max_cost, neededTime[i]);
+
+            }
+            return (result += sum_cost - max_cost);
+        }
+    }
+}
