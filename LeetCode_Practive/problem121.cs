ed5d81d0f0bc875e1b78b30663965ccebabba63a
mrgucci1 <dalquist.keaton221@gmail.com>
2022-01-29 00:50:50 -0600

    finished problem 121, best time to buy and sell stock. easy difficulty

---
+++ LeetCode_Practive/problem121.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Practive
+{
+    class problem121
+    {
+        public void Main121()
+        {
+            //https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+            int[] nums = { 7, 1, 5, 3, 6, 4 };
+            Console.WriteLine(MaxProfit(nums));
+            Console.ReadKey();
+        }
+        public int MaxProfit(int[] prices)
+        {
+            int buy = int.MaxValue;
+            int sell = 0;
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (prices[i] < buy)
+                    buy = prices[i];
+                if (prices[i] - buy > sell)
+                    sell = prices[i] - buy;
+            }
+            if (sell < 0)
+                return 0;
+            return sell;
+        }
+    }
+}
