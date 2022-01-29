0abf7c32b70f6a9bc3ccc390c468492bb557c039
mrgucci1 <dalquist.keaton221@gmail.com>
2022-01-29 00:26:00 -0600

    finished problem 3, longest substring without repeating characters, medium difficulty. Was able to figure out a brute force technique with the help of debugging

---
+++ LeetCode_Practive/problem3.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Practive
+{
+    class problem3
+    {
+        public void Main3()
+        {
+            //https://leetcode.com/problems/longest-substring-without-repeating-characters
+            Console.WriteLine(LengthOfLongest("bbtablud"));
+            Console.ReadKey();
+        }
+
+        public int LengthOfLongest(string s)
+        {
+            int max = 0;
+            if (s.Length > 0)
+                max = 1;
+            for (int i = 0; i < s.Length; i++)
+            {
+                HashSet<char> hashSet = new HashSet<char>();
+                string compare = s[i].ToString();
+                hashSet.Add(s[i]);
+                for (int j = 1 + i; j < s.Length; j++)
+                {
+                    compare += s[j].ToString();
+                    hashSet.Add(s[j]);
+                    //if our compare = hashset, we know we have unique digits
+                    if (compare.Length == hashSet.Count)
+                    {
+                        max = Math.Max(max, compare.Length);
+                        continue;
+                    }
+                    //we do not have unique chars, break out of inner loop
+                    break;
+                }
+            }
+            return max;
+        }
+    }
+}
