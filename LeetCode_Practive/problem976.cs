c20d6a3a338f864ee98933a887a5f48da634b5e9
mrgucci1 <dalquist.keaton221@gmail.com>
2022-02-02 22:11:02 -0600

    finished problem 976, Largest Perimeter Triangle, easy difficulty. Was working on a solution involving hashmaps but didnt stop to thing of what would make up a triangle

---
+++ LeetCode_Practive/problem976.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Practive
+{
+    class problem976
+    {
+        public void Main976()
+        {
+            int[] nums = { 1, 5, 4, 6, 7, 8, 4, 3, 2, 4, 7, 5, 4, 2, 5, 6, 7, 8, 9, 4, 3, 2, 4, 5, 6, 32, 4353, 235, 62, 32, 31 };
+            Console.WriteLine(LargestPerimeter(nums));
+            Console.ReadKey();
+        }
+        public int LargestPerimeter(int[] nums)
+        {
+            //sort array
+            Array.Sort(nums);
+            //go through sorted array backwords
+            for (int i = nums.Length - 3; i >= 0; i--)
+            {
+                //if a + b > c then we have a valid triangle, on first iteration
+                //a = 3rd largest number, b = 2nd largest number, and c = largest number. 
+                //loop until we satisfy a+b>c
+                int a = nums[i], b = nums[i + 1], c = nums[i + 2];
+                if (a + b > c)
+                    return a + b + c;
+            }
+            return 0;
+        }
+    }
+}
