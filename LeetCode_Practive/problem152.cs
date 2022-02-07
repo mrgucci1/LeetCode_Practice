44378ca946f16b0a304512297d83ba80167880b0
mrgucci1 <dalquist.keaton221@gmail.com>
2022-02-06 22:32:14 -0600

    finished problem 152, maximum product subarry. medium difficulty

---
+++ LeetCode_Practive/problem152.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Practive
+{
+    class problem152
+    {
+        public void Main152()
+        {
+            //https://leetcode.com/problems/maximum-product-subarray/
+            //help from
+            //https://www.youtube.com/watch?v=lXVy6YWFcRM
+            //https://stackoverflow.com/questions/6800838/is-there-a-method-to-find-the-max-of-3-numbers-in-c
+            int[] nums = { -4, -3, -2 };
+            Console.WriteLine(MaxProduct(nums));
+            Console.Read();
+        }
+        public int MaxProduct(int[] nums)
+        {
+            int result = nums.Max();
+            int currMin = 1, currMax = 1;
+            foreach (int num in nums)
+            {
+                int temp = currMax * num;
+                currMax = Max(num * currMax, num * currMin, num);
+                currMin = Min(temp, num * currMin, num);
+                result = Max(result, currMax, currMin);
+            }
+            return result;
+        }
+        static int Max(params int[] numbers)
+        {
+            return numbers.Max();
+        }
+        static int Min(params int[] numbers)
+        {
+            return numbers.Min();
+        }
+    }
+}
