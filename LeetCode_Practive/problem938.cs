a7a75b00658cc0474b2c32c42240aebfeb5b3637
mrgucci1 <dalquist.keaton221@gmail.com>
2022-03-03 15:24:09 -0600

    finished problem 938, range sume of bst, easy difficulty

---
+++ LeetCode_Practive/problem938.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Practive
+{
+    internal class problem938
+    {
+        int count = 0;
+        public void Main938()
+        {
+            //https://leetcode.com/problems/range-sum-of-bst/
+            //worked with Will Taylor
+        }
+        public int RangeSumBST(TreeNode root, int low, int high)
+        {
+            helper(root, low, high);
+            return count;
+        }
+        public void helper(TreeNode root, int low, int high)
+        {
+            if (root == null)
+                return;
+            if (root.val >= low && root.val <= high)
+                count += root.val;
+            helper(root.left, low, high);
+            helper(root.right, low, high);
+
+        }
+    }
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
